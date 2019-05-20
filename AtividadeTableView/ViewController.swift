92e2cabd66ac96b89114715703e1af5aec12d08b
LunaFelipe <felipe.luna.tersi@gmail.com>
2019-05-20 09:49:39 -0300

    Initial Commit

---
+++ AtividadeTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AtividadeTableView
+//
+//  Created by Felipe Luna Tersi on 20/05/19.
+//  Copyright © 2019 Felipe Luna Tersi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
